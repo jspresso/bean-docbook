18662ac0a125e772341d028573bc0201055f53ba
Vincent Vandenschrick <vvandens@jspresso.org>
2009-11-05 08:32:54 +0000

    Bean docbook 1st commit

---
+++ src/main/java/org/jspresso/beandocbook/BeanDocbookDoclet.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2005-2009 Vincent Vandenschrick. All rights reserved.
+ *
+ *  This file is part of the Jspresso framework.
+ *
+ *  Jspresso is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Jspresso is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Jspresso.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jspresso.beandocbook;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.LanguageVersion;
+import com.sun.javadoc.MethodDoc;
+import com.sun.javadoc.Parameter;
+import com.sun.javadoc.ParameterizedType;
+import com.sun.javadoc.RootDoc;
+import com.sun.javadoc.Type;
+
+/**
+ * A simple doclet to generate docbook from javadoc information.
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class BeanDocbookDoclet {
+
+  private static String rootClassName;
+  private static int    indent = 0;
+  private static Writer writer;
+
+  /**
+   * Generate docbook part documenting beans.
+   * 
+   * @param root
+   *          the root doc.
+   * @return true if succesful.
+   */
+  public static boolean start(RootDoc root) {
+    Map<ClassDoc, ClassTree> classTrees = new LinkedHashMap<ClassDoc, ClassTree>();
+    ClassTree rootClassTree = null;
+    readOptions(root.options());
+    ClassDoc[] classes = root.classes();
+    try {
+      writer = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
+      for (ClassDoc classDoc : classes) {
+        if (classDoc.isPublic()) {
+          ClassTree classTree = new ClassTree(classDoc);
+          if (rootClassName.equals(classDoc.qualifiedName())) {
+            rootClassTree = classTree;
+          }
+          classTrees.put(classDoc, classTree);
+        }
+      }
+      if (rootClassTree != null) {
+        for (Map.Entry<ClassDoc, ClassTree> entry : classTrees.entrySet()) {
+          ClassTree parent = classTrees.get(entry.getKey().superclassType());
+          if (parent != null) {
+            parent.getSubclasses().add(entry.getValue());
+          }
+        }
+        processClassTree(rootClassTree);
+      }
+      writer.flush();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    return true;
+  }
+
+  private static void processClassTree(ClassTree classTree) throws IOException {
+    writeLine("<section>");
+    indent++;
+    ClassDoc classDoc = classTree.getRoot();
+    processClassDoc(classDoc);
+    for (ClassTree subclassTree : classTree.getSubclasses()) {
+      processClassTree(subclassTree);
+    }
+    indent--;
+    writeLine("</section>");
+  }
+
+  private static void processClassDoc(ClassDoc classDoc) throws IOException {
+    writeLine("<title>" + classDoc.name() + "</title>");
+    writeLine("<para>" + classDoc.commentText() + "</para>");
+    writeLine("<para>Package : " + classDoc.containingPackage().name()
+        + "</para>");
+    writeLine("<para>Properties :</para>");
+    writeLine("<table>");
+    indent++;
+    writeLine("<title>" + classDoc.name() + " properties</title>");
+    writeLine("<tgroup cols=\"3\">");
+    indent++;
+    writeLine("<colspec colname=\"name\" colwidth=\"1*\" />");
+    writeLine("<colspec colname=\"type\" colwidth=\"1*\" />");
+    writeLine("<colspec colname=\"description\" colwidth=\"4*\" />");
+    writeLine("<thead>");
+    indent++;
+    writeLine("<row>");
+    indent++;
+    writeLine("<entry align=\"center\">Name</entry>");
+    writeLine("<entry align=\"center\">Type</entry>");
+    writeLine("<entry align=\"center\">Description</entry>");
+    indent--;
+    writeLine("</row>");
+    indent--;
+    writeLine("</thead>");
+    writeLine("<tbody>");
+    indent++;
+    for (MethodDoc methodDoc : classDoc.methods()) {
+      if (methodDoc.isPublic() && isSetter(methodDoc)) {
+        writeLine("<row>");
+        indent++;
+        String property = getProperty(methodDoc);
+        writeLine("<entry>" + property + "</entry>");
+        Parameter param = methodDoc.parameters()[0];
+        ParameterizedType pType = param.type().asParameterizedType();
+        if (pType != null) {
+          Type[] typeArguments = pType.asParameterizedType().typeArguments();
+          StringBuffer buff = new StringBuffer();
+          buff.append(param.type().simpleTypeName());
+          buff.append("&lt;");
+          for (int i = 0; i < typeArguments.length; i++) {
+            buff.append(typeArguments[i].simpleTypeName());
+            if (i < typeArguments.length - 1) {
+              buff.append(",");
+            }
+          }
+          buff.append("&gt;");
+          writeLine("<entry>" + buff.toString() + "</entry>");
+        } else {
+          writeLine("<entry>" + param.type().simpleTypeName() + "</entry>");
+        }
+        writeLine("<entry>" + methodDoc.commentText() + "</entry>");
+        indent--;
+        writeLine("</row>");
+      }
+    }
+    indent--;
+    writeLine("</tbody>");
+    indent--;
+    writeLine("</tgroup>");
+    indent--;
+    writeLine("</table>");
+  }
+
+  private static String getProperty(MethodDoc methodDoc) {
+    return methodDoc.name().substring(3, 4).toLowerCase()
+        + methodDoc.name().substring(4);
+  }
+
+  private static boolean isSetter(MethodDoc methodDoc) {
+    return methodDoc.name().startsWith("set");
+  }
+
+  private static void writeLine(String content) throws IOException {
+    for (int i = 0; i < indent; i++) {
+      writer.write("  ");
+    }
+    writer.write(content);
+    writer.write("\n");
+  }
+
+  /**
+   * Indicates this doclet supports 1.5 sources.
+   * 
+   * @return LanguageVersion.JAVA_1_5
+   */
+  public static LanguageVersion languageVersion() {
+    return LanguageVersion.JAVA_1_5;
+  }
+
+  private static void readOptions(String[][] options) {
+    for (int i = 0; i < options.length; i++) {
+      String[] opt = options[i];
+      if (opt[0].equals("-rootClassName")) {
+        rootClassName = opt[1];
+      }
+    }
+  }
+
+  /**
+   * Mandatory for custom options
+   * 
+   * @param option
+   *          the custom option.
+   * @return the length of the option including the option itself.
+   */
+  public static int optionLength(String option) {
+    if (option.equals("-rootClassName")) {
+      return 2;
+    }
+    return 0;
+  }
+
+}
---
+++ src/main/java/org/jspresso/beandocbook/ClassTree.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2005-2009 Vincent Vandenschrick. All rights reserved.
+ *
+ *  This file is part of the Jspresso framework.
+ *
+ *  Jspresso is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Jspresso is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Jspresso.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jspresso.beandocbook;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.sun.javadoc.ClassDoc;
+
+/**
+ * Data structure to represent a class tree.
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class ClassTree {
+
+  private ClassDoc       root;
+  private Set<ClassTree> subclasses;
+
+  /**
+   * Constructs a new <code>ClassTree</code> instance.
+   * 
+   * @param root
+   *          the root of this class tree.
+   */
+  public ClassTree(ClassDoc root) {
+    this.root = root;
+    this.subclasses = new LinkedHashSet<ClassTree>();
+  }
+
+  /**
+   * Gets the root.
+   * 
+   * @return the root.
+   */
+  public ClassDoc getRoot() {
+    return root;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toString() {
+    return root.toString();
+  }
+
+  /**
+   * Gets the subclasses.
+   * 
+   * @return the subclasses.
+   */
+  public Set<ClassTree> getSubclasses() {
+    return subclasses;
+  }
+}
